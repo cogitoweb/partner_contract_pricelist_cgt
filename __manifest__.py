208fab7a00ed0148c99edcf5a33dbf724ce8659a
fabiomix <fabiomix@users.noreply.github.com>
2017-08-21 16:15:18 +0200

    Migration from 8.0 to 10.0

---
+++ __manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Cogitoweb (<http://cogitoweb.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Partner Contract Pricelist",
+    'summary': "Partner Contract Pricelist",
+    
+    'description': """
+        This module adds following functionality :
+            1) It adds "Product Pricelist" tab in Sales/Sales/Contracts form to configure Products.
+    """,
+    
+    'author': "CogitoWeb",
+    'website': "https://www.cogitoweb.it",
+
+    'category': "Tools",
+    'version': "10.0.2.1",
+    
+    # any module necessary for this one to work correctly
+    'depends': ['stock'], #account_analytic_analysis
+    
+    # always loaded
+    'data': [
+        'views/account_analytic_account.xml',
+        'security/ir.model.access.csv',
+    ],
+    
+    'installable': True,
+    'auto_install': False,
+}
--- models/account_analytic_account.py
+++ models/account_analytic_account.py
@@ -1,11 +1,12 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
-from openerp import models, fields, api
-from openerp.exceptions import ValidationError
-from openerp.tools.translate import _
 
 class AnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
+
 
     @api.one
     @api.constrains('pricelist_ids')
@@ -18,14 +19,17 @@ class AnalyticAccount(models.Model):
     pricelist_ids = fields.One2many('sale.contract.pricelist', 'analytic_account_id', 'Pricelists')
 
 
+
 class SaleContractPricelist(models.Model):
     _name = "sale.contract.pricelist"
     _description = "Sales Contract Pricelist"
     _order = "sequence"
 
+
     @api.onchange('product_id')
     def onchange_product(self):
         self.update({'product_uom_id': self.product_id.product_tmpl_id.uom_id.id})
+
 
     analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', ondelete="cascade")
     product_id = fields.Many2one('product.product', 'Product', domain="[('type','in',('product', 'consu'))]")
