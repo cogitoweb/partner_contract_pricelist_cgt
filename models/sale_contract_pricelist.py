e4f1b5b6347bcd1fcdd644144d407e2a7453b3f8
fabiomix <fabiomix@users.noreply.github.com>
2017-10-27 15:48:27 +0200

    added sell_discount field

--- models/sale_contract_pricelist.py
+++ models/sale_contract_pricelist.py
@@ -14,10 +14,17 @@ class SaleContractPricelist(models.Model):
     def onchange_product(self):
         self.update({'product_uom_id': self.product_id.product_tmpl_id.uom_id.id})
 
+    @api.one
+    @api.constrains('sell_discount')
+    def _check_description(self):
+        if self.sell_discount < 0 or self.sell_discount > 100:
+            raise ValidationError("Sell Discount must be between 0 and 100")
+
 
     analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', ondelete="cascade")
     product_id = fields.Many2one('product.product', 'Product', domain="[('type','in',('product', 'consu'))]")
     product_uom_id = fields.Many2one('product.uom', 'Product UOM')
-    sell_price = fields.Float('Sell Price')
     minimum_stock_qty = fields.Integer('Minimum Stock Quantity')
+    sell_price = fields.Float('Sell Price')
+    sell_discount = fields.Float('Sell Discount (%)', digits=(6,3), default=0.0)
     sequence = fields.Integer('Sequence')
