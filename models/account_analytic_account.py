f17b6781ddde9b6348daa8247dce940539311924
2zx <2zx@users.noreply.github.com>
2018-03-02 18:52:40 +0100

    remove validation on min quant

--- models/account_analytic_account.py
+++ models/account_analytic_account.py
@@ -7,13 +7,4 @@ from odoo.tools.translate import _
 class AnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
-
-    @api.one
-    @api.constrains('pricelist_ids')
-    def check_qty(self):
-        for rec in self:
-            for line in rec.pricelist_ids:
-                if line.minimum_stock_qty <= 0:
-                    raise ValidationError(_('Invalid Product Pricelist!\n\nMinimum Stock Quantity must be greater than 0.'))
-
     pricelist_ids = fields.One2many('sale.contract.pricelist', 'analytic_account_id', 'Pricelists')
