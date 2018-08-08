d5bfbbc8ac5418c1f1a9e6be302550da760d8051
2zx <2zx@users.noreply.github.com>
2018-08-08 09:44:23 +0200

    product_uom_id is related

--- models/sale_contract_pricelist.py
+++ models/sale_contract_pricelist.py
@@ -9,21 +9,15 @@ class SaleContractPricelist(models.Model):
     _description = "Sales Contract Pricelist"
     _order = "sequence"
 
-
-    @api.onchange('product_id')
-    def onchange_product(self):
-        self.update({'product_uom_id': self.product_id.product_tmpl_id.uom_id.id})
-
     @api.one
     @api.constrains('sell_discount')
     def _check_description(self):
         if self.sell_discount < 0 or self.sell_discount > 100:
             raise ValidationError("Sell Discount must be between 0 and 100")
 
-
     analytic_account_id = fields.Many2one(comodel_name='account.analytic.account', string='Analytic Account', ondelete="cascade")
-    product_id = fields.Many2one(comodel_name='product.product', string='Product', domain="[('type', 'in', ('product', 'consu'))]")
-    product_uom_id = fields.Many2one(comodel_name='product.uom', string='Product UOM')
+    product_id = fields.Many2one(comodel_name='product.product', string='Product')
+    product_uom_id = fields.Many2one(related='product_id.product_tmpl_id.uom_id', store=True)
     minimum_stock_qty = fields.Integer(string='Minimum Stock Quantity')
     description = fields.Char(string='Description')
     sell_price = fields.Float(string='Sell Price')
