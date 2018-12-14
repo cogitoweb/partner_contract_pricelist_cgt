45c00e230fe7f7fdcca689057c780a5aa2064b24
2zx <paolo.cazzitti@cogitoweb.it>
2018-12-14 15:19:21 +0100

    fix domain

--- models/product_template.py
+++ models/product_template.py
@@ -27,10 +27,6 @@ class ProductTemplate(models.Model):
         action = self.env.ref('partner_contract_pricelist_cgt.sale_contract_pricelist_action')
         product_ids = self.with_context(active_test=False).product_variant_ids.ids
 
-        # sequences can be copied by slicing
-        new_domain = action.domain[:]
-        new_domain.append(('product_id.product_tmpl_id', '=', self.id))
-
         return {
             'name': action.name,
             'help': action.help,
@@ -40,5 +36,8 @@ class ProductTemplate(models.Model):
             'target': action.target,
             'context': "{'default_product_id': " + str(product_ids[0]) + "}",
             'res_model': action.res_model,
-            'domain': new_domain
+            'domain': [
+                ('analytic_account_id.active', '=', True),
+                ('product_id.product_tmpl_id', '=', self.id)
+            ]
         }
