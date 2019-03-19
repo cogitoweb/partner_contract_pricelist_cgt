82bc9d6d0cd7316b950c9f7154985dd394a0342b
fabiomix <fabiomix@users.noreply.github.com>
2019-03-19 14:45:00 +0100

    action_add_to_pricelist

--- wizard/pricelist_from_order_line.py
+++ wizard/pricelist_from_order_line.py
@@ -37,6 +37,10 @@ class PricelistPricelistFromOrderLine(models.TransientModel):
         default=_get_contract_id,
     )
 
+    pipe_list_ids = fields.Char(
+        string='list_ids',
+    )
+
 
     # Constraints and onchanges
 
@@ -44,6 +48,13 @@ class PricelistPricelistFromOrderLine(models.TransientModel):
     def _onchange_order_id(self):
         line_ids = self._get_order_line_ids(self.order_id)
         self.order_line_ids = line_ids.ids
+
+
+    @api.onchange('order_line_ids')
+    def _onchange_order_line_ids(self):
+        # can't use self.order_line_ids in action, so we create
+        # a piped string with the ids
+        self.pipe_list_ids = '|'.join([str(x) for x in self.order_line_ids.ids])
 
 
     def _get_order_line_ids(self, order_id):
@@ -73,4 +84,63 @@ class PricelistPricelistFromOrderLine(models.TransientModel):
 
     @api.multi
     def action_add_to_pricelist(self):
-        pass
+        Pricelist = self.env['sale.contract.pricelist']
+        SaleOrderLine = self.env['sale.order.line']
+
+        # nothing to add
+        if not self.pipe_list_ids:
+            return {
+                'type': 'ir.actions.act_window.message',
+                'title': _('Message'),
+                'message': _("No sale order lines selected."),
+                'close_button_title': _('Close')
+            }
+
+        # this should never happen
+        if not self.contract_id:
+            return {
+                'type': 'ir.actions.act_window.message',
+                'title': _('Message'),
+                'message': _("No contract selected."),
+                'close_button_title': _('Close')
+            }
+
+        # cant use self.order_line_ids directly since at this
+        # point is empty for some reason. Used onchange() to convert
+        # self.order_line_ids to list of ids, piped with '|'.
+        # here we split the string, convert values to int and browse them.
+        list_of_ids = self.pipe_list_ids.split('|')
+        list_of_int_ids = map(int, list_of_ids)
+        order_line_ids = SaleOrderLine.browse(list_of_int_ids)
+
+        # filter out already added lines
+        linked_pricelist_ids = Pricelist.search([
+            ('order_line_id', 'in', order_line_ids.ids)
+        ])
+
+        order_line_to_exlude_ids = linked_pricelist_ids.mapped('order_line_id')
+        order_line_to_add_ids = order_line_ids - order_line_to_exlude_ids
+
+        # create pricelist lines
+        for order_line in order_line_to_add_ids:
+            Pricelist.create({
+                'analytic_account_id': self.contract_id.id,
+                'product_id': order_line.product_id.id,
+                'description': order_line.name,
+                'product_uom_id': order_line.product_uom.id,
+                'minimum_stock_qty': order_line.product_uom_qty,
+                'sell_price': order_line.price_unit,
+                'sell_discount': order_line.discount,
+                'order_line_id': order_line.id
+            })
+
+        return {
+            'type': 'ir.actions.act_window.message',
+            'title': _('Message'),
+            'message': _("Lines from sale order added to contract pricelist"),
+            'close_button_title': False,
+            'buttons': [{
+                'type': 'ir.actions.act_window_close',
+                'name': _('Close')
+            }]
+        }
