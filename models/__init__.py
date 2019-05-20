3ac31fac6574e46b2f0268c612badcb6ad0239ec
fabiomix <fabiomix@users.noreply.github.com>
2019-05-20 11:37:43 +0200

    typo

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@ import account_analytic_account
 import product_product
 import product_template
 import sale_contract_pricelist
+import sale_order_line
 import sale_order
 import stock_picking_package_preparation
