9791f520ea809235b9f83eb3a6e2ef16548f60f3
2zx <2zx@users.noreply.github.com>
2019-10-12 21:23:12 +0200

    fix migration if order_line_id does not exist in sale_contract_pricelist

--- migrations/10.0.2.2.0/post-update.py
+++ migrations/10.0.2.2.0/post-update.py
@@ -11,16 +11,32 @@ def migrate(cr, version):
         "Running post-migrate script. Old version was %s"
         % version
     )
-
-    # from v10.0.2.2.0, reference between sale_order_line and
-    # sale_contract_pricelist are not saved on scp but in sol
+    
     cr.execute("""
-        UPDATE sale_order_line
-        SET pricelist_id = subquery.pricelist_id
-        FROM (
-            SELECT scp.id AS pricelist_id, sol.id AS order_line_id
-            FROM sale_contract_pricelist scp
-            INNER JOIN sale_order_line sol ON scp.order_line_id = sol.id
-        ) AS subquery
-        WHERE id = subquery.order_line_id
+        SELECT column_name 
+        FROM information_schema.columns 
+        WHERE table_name='sale_contract_pricelist' and column_name='order_line_id'
     """)
+    
+    column_exists = cr.dictfetchone()
+    
+    if column_exists and column_exists['column_name']:
+
+        # from v10.0.2.2.0, reference between sale_order_line and
+        # sale_contract_pricelist are not saved on scp but in sol
+        cr.execute("""
+            UPDATE sale_order_line
+            SET pricelist_id = subquery.pricelist_id
+            FROM (
+                SELECT scp.id AS pricelist_id, sol.id AS order_line_id
+                FROM sale_contract_pricelist scp
+                INNER JOIN sale_order_line sol ON scp.order_line_id = sol.id
+            ) AS subquery
+            WHERE id = subquery.order_line_id
+        """)
+        
+    else:
+
+        _logger.info(
+            "order_line_id does not exist in sale_contract_pricelist, skip migration"
+        )
